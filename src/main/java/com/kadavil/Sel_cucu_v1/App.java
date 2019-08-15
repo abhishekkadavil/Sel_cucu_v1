590309be023dbac7a8f5ae0ac4af324e887a89dd
abhishekkadavil <52367312+abhishekkadavil@users.noreply.github.com>
2019-08-15 12:37:46 +0530

    first commit
    first commit

---
+++ src/main/java/com/kadavil/Sel_cucu_v1/App.java
@@ -0,0 +1,13 @@
+package com.kadavil.Sel_cucu_v1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
